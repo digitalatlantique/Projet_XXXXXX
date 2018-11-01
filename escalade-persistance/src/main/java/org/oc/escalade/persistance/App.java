bb5c1a3c6ec59d2e3eccd853c558d200dba0660f
digitalatlantique <mick.info@yahoo.com>
2018-11-01 16:55:47 +0100

    Modification des dépendances du pom.xml escalade et persistance

--- escalade-persistance/src/main/java/org/oc/escalade/persistance/App.java
+++ escalade-persistance/src/main/java/org/oc/escalade/persistance/App.java
@@ -3,11 +3,13 @@ package org.oc.escalade.persistance;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 /**
  * Hello world!
  *
  */
+@Component
 public class App{	
 	
     public static void main( String[] args ){
